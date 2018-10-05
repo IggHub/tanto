f169d88c68c6ecd9bbd2e2651497dc70d6b96ac9
Iggy <igoririanto@rocketmail.com>
2018-10-05 17:26:07 -0500

    add cover_image

---
+++ priv/repo/migrations/20181005222349_create_cover_image.exs
@@ -0,0 +1,12 @@
+defmodule Tanto.Repo.Migrations.CreateCoverImage do
+  use Ecto.Migration
+
+  def change do
+    create table(:cover_image) do
+      add :file_name, :string
+      add :recipe_id, references(:recipes, on_delete: :delete_all)
+    end
+
+    create index(:cover_image, [:recipe_id])
+  end
+end
