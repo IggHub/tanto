7a550e1c7456d64ab31d83366547c1bee37e3392
Iggy <igoririanto@rocketmail.com>
2018-10-06 10:59:46 -0500

    add user profiles

---
+++ priv/repo/migrations/20181006155825_create_user_profiles.exs
@@ -0,0 +1,13 @@
+defmodule Tanto.Repo.Migrations.CreateUserProfiles do
+  use Ecto.Migration
+
+  def change do
+    create table(:user_profiles) do
+      add :name, :string
+      add :email, :string
+      add :user_id, references(:users, on_delete: :delete_all)
+    end
+
+    create index(:user_profiles, [:user_id])
+  end
+end
