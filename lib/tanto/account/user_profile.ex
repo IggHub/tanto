702024e1eb061bf3c3bbe124d6320d018f185c56
Iggy <igoririanto@rocketmail.com>
2018-10-10 17:55:50 -0500

    user profile changeset, validate_confirmation email

--- lib/tanto/account/user_profile.ex
+++ lib/tanto/account/user_profile.ex
@@ -1,8 +1,16 @@
 defmodule Tanto.Account.UserProfile do
   use Ecto.Schema
-
+  alias Tanto.Account.UserProfile
   schema "user_profiles" do
     field :name, :string
     field :email, :string
+    
+    timestamp
+  end
+
+  def changeset(%UserProfile{} = user_profile, attrs) do
+    recipe
+    |> cast(data, params[:email])
+    |> validate_confirmation(:email, message: "does not match email")
   end
 end
