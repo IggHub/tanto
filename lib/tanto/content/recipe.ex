c564befc7f59365207310598850e9fccf2759086
Iggy <igoririanto@rocketmail.com>
2018-10-10 17:48:49 -0500

    add recipe changeset and require title

--- lib/tanto/content/recipe.ex
+++ lib/tanto/content/recipe.ex
@@ -1,5 +1,7 @@
 defmodule Tanto.Content.Recipe do
   use Ecto.Schema
+  import Ecto.Changeset
+  alias Tanto.Content.Recipe
 
   schema "recipes" do
     field :title, :string
@@ -13,6 +15,14 @@ defmodule Tanto.Content.Recipe do
     has_many :recipe_translations, Tanto.Content.RecipeTranslation
     has_many :comments, Tanto.Content.Comment
     has_many :cover_images, Tanto.Content.CoverImage
+  
+    timestamps()
+  end
+  
+  def changeset(%Recipe{} = recipe, attrs) do
+    recipe
+    |> cast(attrs, [:title])
+    |> validate_required([:title])
   end
 end
 
